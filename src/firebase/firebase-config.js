d2a2567ce850d51374d0ee976882f2ad753d10f1
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-07-05 20:51:26 +0700

    1 - ThietLapFirebase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWotAhBf6GxhS2sXvIChVEqZQbfLAfafE",
+    authDomain: "monkey-blogging-eabe0.firebaseapp.com",
+    projectId: "monkey-blogging-eabe0",
+    storageBucket: "monkey-blogging-eabe0.appspot.com",
+    messagingSenderId: "532389126285",
+    appId: "1:532389126285:web:2cb7fa1742fcc2d62482c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
