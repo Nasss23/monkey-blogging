95583697e85abe72908004c12022e5a3049a59dc
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-07-17 19:29:24 +0700

    25 - UpdateTogglePassword

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -13,98 +13,81 @@ import { IconEyeClose, IconEyeOpen } from '../components/icon';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase-config';
 import { useAuth } from '../contexts/auth-context';
+import InputPasswordToggle from '../components/input/InputPasswordToggle';
 
 const schema = yup.object({
     email: yup
         .string()
-        .email("Please enter valid your email")
-        .required("Please enter your email address"),
+        .email('Please enter valid your email')
+        .required('Please enter your email address'),
     password: yup
         .string()
-        .min(8, "Please password must be ad least 8 characters or greater")
-        .required("Please enter your password")
-})
-
+        .min(8, 'Please password must be ad least 8 characters or greater')
+        .required('Please enter your password'),
+});
 
 const SignInPage = () => {
     const {
         handleSubmit,
         control,
-        formState: {
-            isValid,
-            isSubmitting,
-            errors
-        }
+        formState: { isValid, isSubmitting, errors },
     } = useForm({
-        mode: "onChange",
-        resolver: yupResolver(schema)
+        mode: 'onChange',
+        resolver: yupResolver(schema),
     });
     useEffect(() => {
-        const arrayError = Object.values(errors)
+        const arrayError = Object.values(errors);
         if (arrayError.length > 0) {
             toast.error(arrayError[0]?.message, {
                 pauseOnHover: false,
-                delay: 100
-            })
+                delay: 100,
+            });
         }
-    }, [errors])
+    }, [errors]);
 
     const { userInfo } = useAuth();
     const navigate = useNavigate();
     useEffect(() => {
-        document.title = "Login Page";
+        document.title = 'Login Page';
         if (userInfo?.email) navigate('/');
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, []);
+
     const handleSignIn = async (values) => {
         if (!isValid) return;
         await signInWithEmailAndPassword(auth, values.email, values.password);
-        navigate("/")
-    }
-    const [togglePassword, setTogglePassword] = useState(false);
+        navigate('/');
+    };
     return (
         <AuthenticationPage>
             <form
                 className='form'
                 onSubmit={handleSubmit(handleSignIn)}
-                autoComplete='off'
-            >
+                autoComplete='off'>
                 <Field>
                     <Label htmlFor='email'>Email address</Label>
                     <Input
                         name='email'
-                        placeholder="Enter your email address"
-                        control={control}
-                    ></Input>
+                        placeholder='Enter your email address'
+                        control={control}></Input>
                 </Field>
                 <Field>
                     <Label htmlFor='password'>Password</Label>
-                    <Input
-                        type={togglePassword ? 'text' : 'password'}
-                        name='password'
-                        placeholder="Enter your password "
-                        control={control}
-                    >
-                        {!togglePassword ? <IconEyeClose
-                            onClick={() => setTogglePassword(true)}
-                        ></IconEyeClose> : <IconEyeOpen
-                            onClick={() => setTogglePassword(false)}
-                        ></IconEyeOpen>
-                        }
-                    </Input>
+                    <InputPasswordToggle control={control}></InputPasswordToggle>
                 </Field>
-                <div className="have-account">
-                    You have not had an account <NavLink to={"/sign-up"}>Register an account</NavLink>
+                <div className='have-account'>
+                    You have not had an account{' '}
+                    <NavLink to={'/sign-up'}>Register an account</NavLink>
                 </div>
                 <Button
-                    type='submit' style={{
+                    type='submit'
+                    style={{
                         width: '100%',
                         maxWidth: 350,
-                        margin: '0 auto'
+                        margin: '0 auto',
                     }}
                     isLoading={isSubmitting}
-                    disabled={isSubmitting}
-                >
+                    disabled={isSubmitting}>
                     Sign Up
                 </Button>
             </form>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,142 +1,119 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
 import { Label } from '../components/label';
 import Input from '../components/input/Input';
-import { useForm } from 'react-hook-form'
-import { IconEyeClose, IconEyeOpen } from '../components/icon';
+import { useForm } from 'react-hook-form';
 import Field from '../components/field/Field';
 import { Button } from '../components/button';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { toast } from 'react-toastify';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase/firebase-config';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../firebase/firebase-config';
 import AuthenticationPage from './AuthenticationPage';
-
-
+import InputPasswordToggle from '../components/input/InputPasswordToggle';
 
 const schema = yup.object({
-    fullname: yup
-        .string()
-        .required("Please enter your name"),
+    fullname: yup.string().required('Please enter your name'),
     email: yup
         .string()
-        .email("Please enter valid your email")
-        .required("Please enter your email address"),
+        .email('Please enter valid your email')
+        .required('Please enter your email address'),
     password: yup
         .string()
-        .min(8, "Please password must be ad least 8 characters or greater")
-        .required("Please enter your password")
-})
+        .min(8, 'Please password must be ad least 8 characters or greater')
+        .required('Please enter your password'),
+});
 
 const SignUpPage = () => {
     const navigate = useNavigate();
     const {
         control,
         handleSubmit,
-        formState: {
-            errors,
-            isValid,
-            isSubmitting
-        },
-        watch,
-        reset
+        formState: { errors, isValid, isSubmitting }
     } = useForm({
-        mode: "onChange",
+        mode: 'onChange',
         resolver: yupResolver(schema),
     });
     const handleSignUp = async (values) => {
         if (!isValid) return;
-        console.log(values)
+        console.log(values);
         // return new Promise((resolve) => {
         //     setTimeout(() => {
         //         resolve();
         //     }, 5000)
         // })
-        const user = await createUserWithEmailAndPassword(auth, values.email, values.password)
+        const user = await createUserWithEmailAndPassword(
+            auth,
+            values.email,
+            values.password
+        );
         await updateProfile(auth.currentUser, {
-            displayName: values.fullname
-        })
+            displayName: values.fullname,
+        });
         const colRef = collection(db, 'user');
         await addDoc(colRef, {
             fullname: values.fullname,
             email: values.email,
-            password: values.password
-        })
-        toast.success('Register successfully!!!')
-        navigate("/")
-    }
-    const [togglePassword, setTogglePassword] = useState(false);
+            password: values.password,
+        });
+        toast.success('Register successfully!!!');
+        navigate('/');
+    };
     useEffect(() => {
-        const arrayError = Object.values(errors)
+        const arrayError = Object.values(errors);
         if (arrayError.length > 0) {
             toast.error(arrayError[0]?.message, {
                 pauseOnHover: false,
-                delay: 100
-            })
+                delay: 100,
+            });
         }
     }, [errors]);
     useEffect(() => {
-        document.title = "Register Page"
-    })
+        document.title = 'Register Page';
+    });
     return (
         <AuthenticationPage>
-            <form className='form' onSubmit={handleSubmit(handleSignUp)} autoComplete='off'>
+            <form
+                className='form'
+                onSubmit={handleSubmit(handleSignUp)}
+                autoComplete='off'>
                 <Field>
-                    <Label htmlFor="fullname">
-                        Fullname
-                    </Label>
+                    <Label htmlFor='fullname'>Fullname</Label>
                     <Input
-                        type="text"
+                        type='text'
                         name='fullname'
                         placeholder='Please enter your fullname'
                         control={control}
                     />
                 </Field>
                 <Field>
-                    <Label htmlFor="email">
-                        Email
-                    </Label>
+                    <Label htmlFor='email'>Email</Label>
                     <Input
-                        type="email"
+                        type='email'
                         name='email'
                         placeholder='Please enter your email address'
                         control={control}
                     />
                 </Field>
                 <Field>
-                    <Label htmlFor="password">
-                        Password
-                    </Label>
-                    <Input
-                        type={togglePassword ? 'text' : 'password'}
-                        name='password'
-                        placeholder='Please enter your password'
-                        control={control}
-                    >
-                        {!togglePassword ? <IconEyeClose
-                            onClick={() => setTogglePassword(true)}
-                        ></IconEyeClose> : <IconEyeOpen
-                            onClick={() => setTogglePassword(false)}
-                        ></IconEyeOpen>
-                        }
-                    </Input>
+                    <Label htmlFor='password'>Password</Label>
+                    <InputPasswordToggle control={control}></InputPasswordToggle>
                 </Field>
-                <div className="have-account">You already have an account <NavLink to={"/sign-in"}>Login</NavLink></div>
+                <div className='have-account'>
+                    You already have an account <NavLink to={'/sign-in'}>Login</NavLink>
+                </div>
                 <Button
                     type='submit'
-
                     style={{
                         width: '100%',
                         maxWidth: 350,
-                        margin: '0 auto'
+                        margin: '0 auto',
                     }}
                     isLoading={isSubmitting}
-                    disabled={isSubmitting}
-                >
+                    disabled={isSubmitting}>
                     Sign Up
                 </Button>
             </form>
