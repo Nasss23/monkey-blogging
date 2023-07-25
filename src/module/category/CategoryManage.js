6670f2d11ab254735b4da5979d03c4b0203e87a3
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2023-07-25 19:46:36 +0700

    47 - DeleteCategory

--- src/module/category/CategoryManage.js
+++ src/module/category/CategoryManage.js
@@ -3,10 +3,11 @@ import { Button } from 'components/button';
 import { LabelStatus } from 'components/label';
 import { Table } from 'components/table';
 import { db } from 'firebase-app/firebase-config';
-import { collection, doc, onSnapshot } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDoc, onSnapshot } from 'firebase/firestore';
 import DashboardHeading from 'module/dashboard/DashboardHeading';
 import React, { useEffect, useState } from 'react';
 import { categoryStatus } from 'utils/constants';
+import Swal from 'sweetalert2';
 
 const CategoryManage = () => {
   const [categoryList, setCategoyList] = useState([]);
@@ -23,7 +24,23 @@ const CategoryManage = () => {
       setCategoyList(result);
     });
   }, []);
-  console.log(categoryList);
+  const handleDeleteCategory = async (docId) => {
+    const colRef = doc(db, 'category', docId);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await deleteDoc(colRef);
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+      }
+    });
+  };
   return (
     <div>
       <DashboardHeading title='Categories' desc='Manage your category'>
@@ -62,7 +79,10 @@ const CategoryManage = () => {
                   <div className='flex items-center gap-x-3'>
                     <ActionView></ActionView>
                     <ActionEdit></ActionEdit>
-                    <ActionDelete></ActionDelete>
+                    <ActionDelete
+                      onClick={() =>
+                        handleDeleteCategory(category.id)
+                      }></ActionDelete>
                   </div>
                 </td>
               </tr>
